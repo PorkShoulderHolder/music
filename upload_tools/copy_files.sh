d68c9eb9914594940ae67e28d7ae243511ee2f8c
Sam Royston <sfoxroyston@gmail.com>
2017-01-18 13:01:04 -0500

    organize

--- upload_tools/copy_files.sh
+++
@@ -1,2 +0,0 @@
-sh coapcp.sh "$1" telnet.lua telnet.lua 
-sh coapcp.sh "$1" pin_config.json pin_config.json
--- upload_tools/full_flash.sh
+++
@@ -1,3 +0,0 @@
-python luatool.py --port $1 --src telnet_srv.lua --dest init.lua  --baud 115200 --verbose
-python luatool.py --port $1 --src aps.txt --dest aps.txt  --baud 115200 --verbose
-python luatool.py --port $1 --src coapcp.lua --dest coapcp.lua  --baud 115200 --verbose
